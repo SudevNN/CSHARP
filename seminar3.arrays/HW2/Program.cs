74c4faa9cd83e84a8aa40928508fcf623c6625ff
Sudev Nikolay <SudevNN@mail.ru>
2024-01-17 22:44:26 +0500

    add seminar3.arrays/HW2/

---
+++ seminar3.arrays/HW2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+// Примеры
+// массив [6 7 19 34 3 1 4 7 9 1] => 3
+// массив [1 8 43 4 55 60 3 2 1 3] => 4
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+        //Напишите свое решение здесь
+        int cnt = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    public static void PrintResult(int[] array)
+    {
+        //Напишите свое решение здесь
+        Console.WriteLine(CountEvenItems(array));
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
