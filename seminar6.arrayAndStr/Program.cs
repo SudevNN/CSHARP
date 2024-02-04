5a7584997546d7dfdee3ec080aaba1520084fe7a
Sudev Nikolay <SudevNN@mail.ru>
2024-02-04 21:43:19 +0500

    add seminar6.arrayAndStr/Task1 and Task2

---
+++ seminar6.arrayAndStr/Program.cs
@@ -0,0 +1,30 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+int GetVovelsCount(string str)
+{
+    string vovels = "aoueiy";// Строка с гласными
+    int countVovels = 0; // Количество гласных 
+    foreach (char symbol in str) // Берем каждую букву из str
+    {
+        foreach (char vovel in vovels) // Берем гласные буквы
+        {
+            if (symbol == vovel) // Мы нашли гласную букву
+            {
+                countVovels++;
+                break; 
+            }
+        }
+    }
+    return countVovels; 
+}
+Console.Write("Введите строчку: ");
+string inputString = Console.ReadLine();
+inputString = inputString.ToLower(); // HELLO => hello
+// ToUpper() -> hello => HELLO
+int countVovels = GetVovelsCount(inputString);
+Console.WriteLine($"Количество гласных: {countVovels}");
---
+++ seminar6.arrayAndStr/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿string GetStringFromCharArray (char[] array)
+{
+    string result = string.Empty; // ""
+    foreach (char symbol in array) 
+    // symbol='a',symbol='b',symbol='c',symbol='d'
+    {
+        result += symbol;
+    }
+    return result;
+}
+
+char[] chars = {'1', 'd', '!', '2', 'f'};
+string res = GetStringFromCharArray(chars);
+Console.WriteLine($"Result: {res}");
