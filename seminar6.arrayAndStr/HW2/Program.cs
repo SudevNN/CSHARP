6e7f2e7a350afe87fb97ce7daebacec8a1338ffc
Sudev Nikolay <SudevNN@mail.ru>
2024-02-04 23:01:11 +0500

    add seminar6.arrayAndStr/HW2

---
+++ seminar6.arrayAndStr/HW2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные.
+// Пример:
+// “aBcD1ef!-” => “abcd1ef!-” 
+
+string RndStr(int letters)
+{
+    Random rnd = new Random();
+    string str = string.Empty;
+    string letterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < letters; i++)
+    {
+        str += letterSet[Convert.ToChar(rnd.Next(0, letterSet.Length))];
+    }
+    return str;
+}
+
+// Уточняем размер строки:
+Console.Write("Введите количество символов: ");
+int lettersQuantity = Convert.ToInt32(Console.ReadLine());
+// Наполняем строку случайными символами
+string createStr = RndStr(lettersQuantity);
+Console.WriteLine(createStr);
+Console.WriteLine("Результат: ");
+string strToLower = createStr.ToLower();
+Console.WriteLine(strToLower);
