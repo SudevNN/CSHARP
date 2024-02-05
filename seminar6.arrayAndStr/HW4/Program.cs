80b1584b72fe10d008ded4e9f0291880dedf1837
Sudev Nikolay <SudevNN@mail.ru>
2024-02-05 21:15:22 +0500

    add seminar6.arrayAndStr/HW4

---
+++ seminar6.arrayAndStr/HW4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+// Пример:
+// “Hello my world” => “world my Hello”
+
+string ReverseWords(string str)
+{
+        string[] words = str.Split(' ');
+        Array.Reverse(words);
+        return string.Join(" ", words);
+}
+// Меняем кодировку для русских символов
+Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+
+Console.Write("Введите предложение: ");
+string sentenceStr = Console.ReadLine();
+string result = ReverseWords(sentenceStr);
+// Вывод результата
+Console.WriteLine(result);
